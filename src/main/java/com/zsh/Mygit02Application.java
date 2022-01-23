c488d4b8a0584d5c2714e33925f8f019d7c1b5c7
Ronin <1817937322@qq.com>
2022-01-23 21:05:29 +0800

    初始化项目

---
+++ src/main/java/com/zsh/Mygit02Application.java
@@ -0,0 +1,13 @@
+package com.zsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mygit02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mygit02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zsh/Mygit02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mygit02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
